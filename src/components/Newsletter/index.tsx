376624db2dc351e4632fd5ebade53a13eee415e9
Diovane Alves <alvdiovane@gmail.com>
2023-07-31 19:26:32 -0300

    Fix: correcting the newsletter padding

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -3,10 +3,10 @@ import NewsletterForm from './NewsletterForm'
 export default function Newsletter() {
   return (
     <div
-      className="container row mx-auto border border-dark my-5 p-4"
+      className="container row mx-auto border border-dark my-5 py-5 px-4"
       style={{ maxWidth: '650px', width: '95%' }}
     >
-      <h5 className="text-center my-4">
+      <h5 className="text-center pb-3">
         Quer receber nossas novidades, promoções exclusivas e 10% OFF na
         primeira compra? <strong>Cadastre-se!</strong>
       </h5>
